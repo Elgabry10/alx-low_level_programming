25102471d4b3a80dcb27fb89a32584b7b5887511
Hassan <hassan1elgabry@gmail.com>
2023-08-10 18:31:27 -0700

    Your commit message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Description: A C program that prints with printf function.
  *
@@ -10,5 +10,6 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
