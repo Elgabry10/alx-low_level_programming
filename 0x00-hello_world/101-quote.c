a7fd6f0c07523abcf845dd47c46e97f1c488d76e
Hassan <hassan1elgabry@gmail.com>
2023-08-11 10:04:01 -0700

    Yoi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <unistd.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
  *
  * Description: print a qoute using write function
- *    ssize_t write(int fd,const void *buf,size_t count)
+ *    ssize_t write(int fd,const void *buf, size_t count);
  *
  * Return: 1 (error).
 */
@@ -13,6 +13,6 @@ int main(void)
 {
 	char quo[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1,quo,59);
+	write(1, quo, 59);
 	return b(1);
 }
